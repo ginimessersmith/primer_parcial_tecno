881a938bd3e64a41e39f8a503609377a0bb33356
carloslaime <carlos.laime.99@gmail.com>
2024-07-03 02:10:39 -0400

    clean main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,6 @@
 package org.example;
 
-import business.BCategorie;
-import business.BState;
-
 public class Main {
     public static void main(String[] args) {
-        // BState bState = new BState();
-        // String result = bState.save("Active");
-        // System.out.println(result);
-        BCategorie bCategorie = new BCategorie();
-        String result = bCategorie.save("Pizza");
-        System.out.println(result);
     }
 }
