d3c4b2cddba48f4a24e5245fef63ed6e8dd43e65
jorgeballivian <jlballi_o@hotmail.com>
2024-07-05 08:51:20 -0400

    correo institucional

--- src/main/java/utils/Extractor.java
+++ src/main/java/utils/Extractor.java
@@ -4,6 +4,7 @@ public class Extractor {
     private static String GMAIL = "d=gmail";
     private static String HOTMAIL = "d=hotmail";
     private static String YAHOO = "d=yahoo";
+    private static String UAGRM = "d=uagrm";
     
     public static Email getEmail(String plain_text) {
         return new Email(getFrom(plain_text), getSubject(plain_text));
@@ -31,30 +32,43 @@ public class Extractor {
             to = getToFromHotmail(plain_text);
         } else if (plain_text.contains(YAHOO)) {
             to = getToFromYahoo(plain_text);
+        } else if (plain_text.contains(UAGRM)) {
+            to = getToFromUagrm(plain_text);
         }
         return to;
     }
     
     private static String getSubject(String plain_text) {
         String search = "Subject: ";
-        int i = plain_text.indexOf(search);
-        if (i == -1) {
+        int index_begin = plain_text.indexOf(search);
+        if (index_begin == -1) {
             return "No Subject";
         }
-        i += search.length();
-        String end_string = "";
-        if (plain_text.contains(GMAIL)) {
-            end_string = "To:";
-        } else if (plain_text.contains(HOTMAIL)) {
-            end_string = "Thread-Topic";
-        } else if (plain_text.contains(YAHOO)) {
-            end_string = "MIME-Version:";
+        index_begin += search.length();
+        int index_end = plain_text.indexOf("\n", index_begin);
+        if (index_end == -1) {
+            // No newline character found, assume subject goes to the end of the string
+            return plain_text.substring(index_begin).trim();
         }
-        int e = plain_text.indexOf(end_string, i);
-        if (e == -1) {
-            return "No Subject";
+    
+        // Continue reading until the next header line or the end of the string
+        StringBuilder subject = new StringBuilder(plain_text.substring(index_begin, index_end).trim());
+        while (true) {
+            int next_line_start = index_end + 1;
+            int next_line_end = plain_text.indexOf("\n", next_line_start);
+            if (next_line_end == -1 || next_line_start >= plain_text.length()) {
+                break;
+            }
+            String next_line = plain_text.substring(next_line_start, next_line_end).trim();
+            if (next_line.isEmpty() || next_line.matches("^[A-Za-z-]+: .*")) {
+                // Stop if the next line is empty or looks like a new header line
+                break;
+            }
+            subject.append(" ").append(next_line);
+            index_end = next_line_end;
         }
-        return plain_text.substring(i, e).trim();
+    
+        return subject.toString().trim();
     }
     
     private static String getToFromGmail(String plain_text) {
@@ -77,6 +91,11 @@ public class Extractor {
             return "Unknown";
         }
         return plain_text.substring(i + 1, e);
+    }
+    
+    private static String getToFromUagrm(String plain_text) {
+        String aux = getToCommon(plain_text);
+        return aux.substring(1, aux.length() - 1);
     }
     
     private static String getToCommon(String plain_text) {
