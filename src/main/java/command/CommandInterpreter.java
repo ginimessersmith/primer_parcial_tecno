33bc1268d9b0e0a00e18386371d09b108c128453
jorgeballivian <jlballi_o@hotmail.com>
2024-07-07 19:59:16 -0400

    cambios del inge

--- src/main/java/command/CommandInterpreter.java
+++ src/main/java/command/CommandInterpreter.java
@@ -10,7 +10,7 @@ public class CommandInterpreter {
     static {
         COMMANDS.put("categories", new String[]{"save", "update", "findOne", "findAll", "delete"});
         COMMANDS.put("orders", new String[]{"save", "update", "findOne", "findAll", "delete"});
-        COMMANDS.put("paymentMethods", new String[]{"save", "update", "findOne", "findAll", "delete"});
+        COMMANDS.put("paymentmethods", new String[]{"save", "update", "findOne", "findAll", "delete"});
         COMMANDS.put("pizzas", new String[]{"save", "update", "findOne", "findAll", "delete"});
         COMMANDS.put("sizes", new String[]{"save", "update", "findOne", "findAll", "delete"});
         COMMANDS.put("usuarios", new String[]{"save", "update", "findOne", "findAll", "delete"});
@@ -120,7 +120,7 @@ public class CommandInterpreter {
                     return HandleUsuarios.findAll();
                 }
                  
-            case "paymentMethods":
+            case "paymentmethods":
                 if(command.equals("save")){
                     return HandlePaymentMethods.save(params);
                 }else if(command.equals("update")){
